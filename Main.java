5b88f5a7717c091a7322a13f36446a7d2f4a8250
itoh <itoh@ccg.ac.jp>
2024-05-16 12:54:47 +0900

    initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(" -- Start --");
+
+        for (String str : args) {
+            System.out.println(str);
+        }
+
+        System.out.println(" -- Finish --");
+    }
+
+}
